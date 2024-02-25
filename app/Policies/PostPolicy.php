b3f2077d61d348c24735df19c3924fa2d009df1a
nimarya <masha.nichiporenko@gmail.com>
2024-02-25 11:08:29 +0300

    added authentication for creating a post

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -59,4 +59,15 @@ class PostPolicy
     {
         return $this->view($user, $post);
     }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Auth\Access\Response|bool
+     */
+    public function create(User $user)
+    {
+        return true;
+    }
 }
